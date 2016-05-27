a1fe08e447137cef5bc31cc36ab38594e980afde
Dongdong Zhou <dzhou121@gmail.com>
2016-05-27 19:31:32 +0100

    first commit

---
+++ rplugin/python/delve.py
@@ -0,0 +1,11 @@
+import neovim
+
+
+@neovim.plugin
+class Main(object):
+    def __init__(self, vim):
+        self.vim = vim
+
+    @neovim.command('DoItPython')
+    def doItPython(self, args):
+        self.vim.command('echo "hello"')
